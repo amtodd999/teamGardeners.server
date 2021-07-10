cd1ba09ded15becd473fc295ca07524b08df96d1
Amelia Heaton <ameliaheaton@MacBook-Pro.local>
2021-07-10 10:57:10 -0400

    amelias branch

--- server/app.js
+++ server/app.js
@@ -2,5 +2,5 @@ const Express = require("express");
 const app = Express();
 
 app.listen(3000, () => {
-    console.log(`[Server]: App is listening on 3000`);
+    console.log(`[Server]: App is listening on 3000, Yay!`);
 });
