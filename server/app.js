c6e0906c7adead6bcf67c3d17931b867e373bdb5
Cindy Blahunka <cindyblahunka@gmail.com>
2021-07-11 00:22:14 -0400

    cindy's usercontroller, validate-jwt and headers

--- server/app.js
+++ server/app.js
@@ -1,6 +1,27 @@
-const Express = require("express");
-const app = Express();
+require("dotenv").config();
+const express = require("express");
+const db = require("./db");
 
-app.listen(3000, () => {
-    console.log(`[Server]: App is listening on 3000`);
-});
+const app = express();
+
+app.use(require("./middleware/headers"));
+
+const controllers = require("./controllers");
+
+app.use(express.json());
+
+app.use("/user", controllers.userController);
+app.use(require("./middleware/validate-jwt"));
+app.use("/notes", controllers.notesController);
+
+db.authenticate()
+  .then(() => db.sync()) // => {force: true} this means delete databases
+  .then(() => {
+    app.listen(3000, () =>
+      console.log(`[Server: ] App is listening on Port ${3000}`)
+    );
+  })
+  .catch((err) => {
+    console.log("[Server: ] Server Crashed");
+    console.error(err);
+  });
