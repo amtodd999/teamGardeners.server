20b894e9ff9c0e86519af7e23eac91365e0266e1
Angie Todd <a.m.todd999@gmail.com>
2021-07-22 20:45:37 -0400

    first heroku deployment

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 const {Sequelize} = require('sequelize');
 
-const db = new Sequelize(process.env.URL_SECRET);
+const db = new Sequelize(process.env.URL_SECRET || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/<dbName>`, {
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'production'
+});
 
 module.exports = db;
